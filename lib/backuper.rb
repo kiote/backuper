5c6c9b321ab546d3947ba0da831f6791c4ebc4c5
kiote <krivich.ekaterina@gmail.com>
2013-11-21 20:00:35 +0400

    to see session

--- lib/backuper.rb
+++ lib/backuper.rb
@@ -5,12 +5,19 @@ class Backuper < Sinatra::Base
   register Sinatra::ConfigFile
   config_file 'config.yml'
 
+  enable :sessions
+
   get '/' do
     "Backups"
+    session['m'] = 'Hello World!'
   end
 
   get '/login' do
     redirect "https://oauth.yandex.ru/authorize?response_type=token&client_id=#{settings.yandex_client_id}"
+  end
+
+  get '/some' do
+    session['m']
   end
 
   # start the server if ruby file executed directly
